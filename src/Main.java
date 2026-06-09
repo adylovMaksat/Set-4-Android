dd0206d6ea993e7df7c967fc7d1a6b4b577f5adc
adylovMaksat <maksatbekadylov@gmail.com>
2026-06-09 23:08:40 +0200

    Upd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-
+    /* Student: Maksatbek Adylov
+       Student ID: 56422
+       Set: Set-4 */
     public static void main(String[] args) {
 
         Rental rental = new Rental(5);
